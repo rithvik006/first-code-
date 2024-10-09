8c45de8730a79d5dcd3edef7f4160a503620fe3b
rithvik006 <katukuririthvik@gmail.com>
2024-10-09 11:05:09 +0530

    Update 2.c

--- 2.c
+++ 2.c
@@ -11,29 +11,29 @@
 
 int main()
 {
-    char operator;
+    char operator;//basic calculator
     double a,b;
 
-    printf("enter any operator(+,-,/,*): ");
-    scanf("%c",&operator);
-    printf("enter two operands:");
-    scanf("%lf%lf",&a,&b);
+    printf("enter any operator(+,-,/,*): ");//asks user operator input
+    scanf("%c",&operator);//reads the input 
+    printf("enter two operands:");//asks user operands
+    scanf("%lf%lf",&a,&b);//read the input
 
     switch(operator)
     {
-        case '+' : printf("%2.lf + %2.lf = %2.lf",a,b,a+b);
+        case '+' : printf("%2.lf + %2.lf = %2.lf",a,b,a+b);//addition and prints the output
         break;
-        case '-' : printf("%2.lf - %2.lf = %2.lf",a,b,a-b);
+        case '-' : printf("%2.lf - %2.lf = %2.lf",a,b,a-b);//subtraction and prints the output
         break;
-        case '*' : printf("%2.lf * %2.lf = %2.lf",a,b,a*b);
+        case '*' : printf("%2.lf * %2.lf = %2.lf",a,b,a*b);//multiplication and prints the output
         break;
-        case '/' :
-            if (b !=0)
+        case '/' ://division and prints the output
+            if (b !=0)//checks the input whether it is 0 or not 
                 printf("%2.lf / %2.lf = %2.lf",a,b,a/b);
             else
                 printf("error!!!,the number is divided by 0");
         break;
-        default : printf("operator is not correct");
+        default : printf("operator is not correct");//prints this output if the input is not an integer
 
     }
 
